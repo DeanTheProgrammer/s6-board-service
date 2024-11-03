0584d1f9a42740a0502f9ee84ada927ba3e79dd1
Dean van Deijck <dean.van.deijck@hotmail.com>
2024-11-03 18:12:43 +0100

    Add InfraRabbitMQ project with RabbitMQ integration
    A new project named "InfraRabbitMQ" has been added to the solution `Project-microservice.sln`. This project is defined in the file `InfraRabbitMQ\InfraRabbitMQ.csproj`.
    
    The build configurations for the new "InfraRabbitMQ" project have been added to the solution, including Debug and Release settings for Any CPU.
    
    The `InfraRabbitMQ.csproj` file targets the .NET 8.0 framework, with ImplicitUsings and Nullable enabled. It includes package references for `Microsoft.Extensions.Options` version 8.0.2 and `RabbitMQ.Client` version 6.8.1.
    
    A new class `RabbitMQSettings` has been added in the `InfraRabbitMQ` namespace, containing a constant string `Settings` and a property `HostName`.
    
    A new class `ProjectSyncHandler` has been added in the `InfraRabbitMQ.Handler.DataSync` namespace. This class uses `IOptions<RabbitMQSettings>` to retrieve RabbitMQ settings, initializes a `ConnectionFactory` with the `HostName` from the settings, and throws an exception if the settings are not found.

---
+++ InfraRabbitMQ/Handler/DataSync/ProjectSyncHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace InfraRabbitMQ.Handler.DataSync
+{
+    public class ProjectSyncHandler
+    {
+        public readonly ConnectionFactory _factory;
+        public ProjectSyncHandler(IOptions<RabbitMQSettings> options)
+        {
+            RabbitMQSettings settings = options.Value;
+            if(settings == null)
+            {
+                throw new Exception("Settings are not found");
+            }
+
+            _factory = new ConnectionFactory { HostName = settings.HostName };
+        }
+
+
+    }
+}
---
+++ InfraRabbitMQ/RabbitMQSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace InfraRabbitMQ
+{
+    public class RabbitMQSettings
+    {
+        public const string Settings = "RabbitMQSettings";
+        public string HostName { get; set; }
+
+    }
+}
