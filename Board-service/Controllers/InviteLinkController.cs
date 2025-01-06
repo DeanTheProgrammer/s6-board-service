eb134d9a83ea1a7d8482f16e9af7b7bb1188b1cf
DeanTheProgrammer <dean.van.deijck@hotmail.com>
2025-01-06 10:39:05 +0100

    Add Auth0 auth, CORS, and user context handling
    Updated InviteLinkController and ProjectController to use IHttpContextAccessor and Auth0AuthorizationHandler for user authorization. Added validation for null HttpContext. Modified Startup.cs to configure CORS policy and added app.UseCors() to middleware. Introduced Auth0AuthorizationHandler for extracting user info and roles from HTTP context. Added AuthorizationUserDTO for encapsulating user information.

--- Board-service/Controllers/InviteLinkController.cs
+++ Board-service/Controllers/InviteLinkController.cs
@@ -1,6 +1,8 @@
-﻿using DTO.DTO_s.InviteLink;
+﻿using Board_service.Handler.AuthorizationHandler;
+using DTO.DTO_s.InviteLink;
 using Microsoft.AspNetCore.Mvc;
 using ProjectService.Handler;
+using System.ComponentModel.DataAnnotations;
 
 namespace Board_service.Controllers
 {
@@ -11,24 +13,59 @@ namespace Board_service.Controllers
         private readonly InviteLinkHandler _handler;
         private readonly string _userId = "TestUserId";
         private readonly ILogger<InviteLinkController> _logger;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
-        public InviteLinkController(InviteLinkHandler handler, ILogger<InviteLinkController> logger)
+        public InviteLinkController(InviteLinkHandler handler, ILogger<InviteLinkController> logger, IHttpContextAccessor httpContextAccessor)
         {
             _handler = handler;
             _logger = logger;
+            _httpContextAccessor = httpContextAccessor;
         }
         [HttpGet]
         public async Task<PublicInviteLinkDTO> GetInvite(string Code)
         {
-            PublicInviteLinkDTO result = await _handler.GetLinkByCode(Code);
+            var HttpContext = _httpContextAccessor.HttpContext;
+            if (HttpContext != null)
+            {
 
-            return result;
+                var UserId = Auth0AuthorizationHandler.GetUserIdFromContext(HttpContext);
+                if (!string.IsNullOrEmpty(UserId))
+                {
+                    PublicInviteLinkDTO result = await _handler.GetLinkByCode(Code);
+                    return result;
+                }
+                else
+                {
+                    throw new UnauthorizedAccessException("You're not authorized");
+                }
+            }
+            else
+            {
+                throw new ValidationException("HttpContext is null");
+            }
         }
         [HttpPost]
         public async Task<InviteLinkDTO> Create([FromBody]CreateInviteLinkDTO dto)
         {
-            InviteLinkDTO result = await _handler.CreateInviteLink(_userId, dto);
-            return result;
+            var HttpContext = _httpContextAccessor.HttpContext;
+            if (HttpContext != null)
+            {
+
+                var UserId = Auth0AuthorizationHandler.GetUserIdFromContext(HttpContext);
+                if (!string.IsNullOrEmpty(UserId))
+                {
+                    InviteLinkDTO result = await _handler.CreateInviteLink(_userId, dto);
+                    return result;
+                }
+                else
+                {
+                    throw new UnauthorizedAccessException("You're not authorized");
+                }
+            }
+            else
+            {
+                throw new ValidationException("HttpContext is null");
+            }
         }
     }
 }
---
+++ Board-service/Handler/AuthorizationHandler/Auth0AuthorizationHandler.cs
@@ -0,0 +1,94 @@
+﻿using CustomExceptions.ObjectExceptions;
+using DTO.DTO_s;
+
+namespace Board_service.Handler.AuthorizationHandler
+{
+    public static class Auth0AuthorizationHandler
+    {
+        public static string GetUserIdFromContext(HttpContext context)
+        {
+            string result = context.Request.Headers["X-User-Id"].ToString();
+            return result;
+        }
+        public static List<string> GetUserRoleFromContext(HttpContext context)
+        {
+            List<string> roles = context.Request.Headers["X-Roles"].ToString().Split(',').ToList();
+            return roles;
+        }
+
+        public static string GetAuth0IdFromContext(HttpContext context)
+        {
+            string result = context.Request.Headers["X-Auth0-Id"].ToString();
+            return result;
+        }
+
+        public static AuthorizationUserDTO GetAllInformationFromContext(HttpContext context)
+        {
+            string UserId = context.Request.Headers["X-User-Id"].ToString();
+            List<string> roles = context.Request.Headers["X-Roles"].ToString().Split(',').ToList();
+            string result = context.Request.Headers["X-Auth0-Id"].ToString();
+            AuthorizationUserDTO user = new AuthorizationUserDTO()
+            {
+                UserId = UserId,
+                Roles = roles,
+                Auth0Id = result
+            };
+            return user;
+        }
+
+        public static bool IsDeleted(HttpContext context)
+        {
+            List<string> roles = GetUserRoleFromContext(context);
+            if (roles != null)
+            {
+                bool result = roles.Contains("Deleted");
+                return result;
+            }
+            else
+            {
+                throw new NotFoundException("Roles not found");
+            }
+        }
+        public static bool IsAdmin(HttpContext context)
+        {
+            List<string> roles = GetUserRoleFromContext(context);
+            if (roles != null)
+            {
+                bool result = roles.Contains("Admin");
+                return result;
+            }
+            else
+            {
+                throw new NotFoundException("Roles not found");
+            }
+        }
+
+        public static bool IsSuperAdmin(HttpContext context)
+        {
+            List<string> roles = GetUserRoleFromContext(context);
+            if (roles != null)
+            {
+                bool result = roles.Contains("SuperAdmin");
+                return result;
+            }
+            else
+            {
+                throw new NotFoundException("Roles not found");
+            }
+        }
+
+        public static bool IsUser(HttpContext context)
+        {
+            List<string> roles = GetUserRoleFromContext(context);
+            if (roles != null)
+            {
+                bool result = roles.Contains("User");
+                return result;
+            }
+            else
+            {
+                throw new NotFoundException("Roles not found");
+            }
+        }
+    }
+}
